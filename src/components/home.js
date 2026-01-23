625271ce3f5ae17c6e37e3ce48750cb2c468ea93
Misha <mishaalam832@gmail.com>
2026-01-23 20:16:03 +0600

    added contact and about part

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,8 @@ export default function Home() {
         <div className="home" id="home">
             <div className="text-section">
                 <h1>Hello, I'm Misha</h1>
-                <p>This is my portfolio website.</p>
+                <p>This is my portfolio</p>
+                <button>Download CV</button>
             </div>
             <div className="image-section">
                 <img src={profileImg} alt="Misha's Profile" />
