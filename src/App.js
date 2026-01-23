e0943e3a062016ff84923577baaca1d88d602833
Misha <mishaalam832@gmail.com>
2026-01-23 16:44:53 +0600

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Navbar from "./components/navbar"
+import Home from "./components/home"
+// import About from "./components/about"
+// import Projects from "./components/projects"
+// import Contact from "./components/contact"
 
 function App() {
   return (
      <div>
        <Navbar />
        <main>
-          <home/>
-          <about/>
-          <projects/>
-          <contact/>
+          <Home/>
+          {/* <About/>
+          <Projects/>
+          <Contact/> */}
        </main>
      </div>
   )
---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import "./home.css"
+import profileImg from "../assets/myPic.png";
+
+export default function Home() {
+    return (
+        <div className="home" id="home">
+            <div className="text-section">
+                <h1>Hello, I'm Misha</h1>
+                <p>This is my portfolio website.</p>
+            </div>
+            <div className="image-section">
+                <img src={profileImg} alt="Misha's Profile" />
+            </div>
+        </div>
+    )
+}
